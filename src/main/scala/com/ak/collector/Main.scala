ddc9eddafe5e5487e560bf30370bff1915a820b2
Algimantas Krasauskas <algimantask@wix.com>
2020-12-19 11:37:51 +0200

    added Procfile

--- src/main/scala/com/ak/collector/Main.scala
+++ src/main/scala/com/ak/collector/Main.scala
@@ -1,12 +1,8 @@
 package com.ak.collector
 
 import cats.effect.{ExitCode, IO, IOApp}
-import scala.io.StdIn.readLine
 
 object Main extends IOApp {
-  def run(args: List[String]) = for {
-    serverFiber <- CollectorServer.stream[IO].compile.drain.start
-    _ <- IO(readLine())
-    _ <- serverFiber.cancel
-  } yield ExitCode.Success
+  def run(args: List[String]): IO[ExitCode] =
+    CollectorServer.stream[IO].compile.drain.as(ExitCode.Success)
 }
